ebed5a59f77c4cae60b6e3a51063b6920f71f6bd
adi pramana <kadekadi368@gmail.com>
2024-06-18 14:35:54 +0800

    update background and padding

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="mt-8 mb-5">
+    <footer className="pt-5 pb-5 bg-[#f8fbfb] dark:bg-[#0d0d0d]">
       <p className="text-base text-bodytext dark:text-abuDark text-center">© {currentYear}, by Laba Adi Pramana</p>
     </footer>
   );
