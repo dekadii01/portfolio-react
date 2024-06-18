ebd39af5c976a76f24cf44c8af9b59db710e991a
adi pramana <kadekadi368@gmail.com>
2024-06-18 14:32:04 +0800

    update position

--- src/components/CardRight/index.jsx
+++ src/components/CardRight/index.jsx
@@ -2,7 +2,7 @@ import MenuItem from "../MenuItem";
 
 const CardRight = () => {
   return (
-    <div className="hidden lg:inline-block lg:col-span-1 bg-white dark:bg-black rounded-xl px-5 z-10 h-fit">
+    <div className="hidden lg:inline-block lg:col-span-1 bg-white dark:bg-black rounded-xl px-5 z-10 h-fit sticky top-5">
       <MenuItem path="/" menu="Home" />
       <MenuItem path="/about" menu="Resume" />
       <MenuItem path="/portfolio" menu="Portfolio" />
